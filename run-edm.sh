c737d9e145b569acae856ff2e70c7144f10a1e26
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-08-30 15:58:06 -0700

    Mount .Xauthority and remove xhost

--- run-edm.sh
+++ run-edm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-xhost + local:docker
-docker run --rm -u $(id -u):$(id -g) -e DISPLAY="$DISPLAY" -h "$HOSTNAME" --network host -v /tmp/.X11-unix:/tmp/.X11-unix slac-epics-client-rocky9:latest edm
+#xhost + local:docker
+docker run --rm -u $(id -u):$(id -g) -e DISPLAY="$DISPLAY" -h "$HOSTNAME" --network host -h $HOSTNAME -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/.Xauthority slac-epics-client-rocky9:latest edm
